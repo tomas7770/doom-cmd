95a05df642b0c07a3feb06f5bbb27bef67b61609
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2024-01-16 21:07:33 +0000

    Implement params option

--- doomcmd.py
+++ doomcmd.py
@@ -5,6 +5,7 @@ import subprocess
 engines = dict()
 
 selected_engine = None
+custom_params = None
 
 def init_config():
     engines_cfg = configparser.ConfigParser()
@@ -18,19 +19,27 @@ def init_config():
 
 def init_args():
     global selected_engine
+    global custom_params
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--engine", help = "source port to use")
+    parser.add_argument("--params", help = "other parameters to pass to the source port")
     args = parser.parse_args()
     if not args.engine:
         raise(NotImplementedError("Default engine not implemented, must specify manually"))
     selected_engine = args.engine
     if not engines.get(selected_engine):
         raise(ValueError("Specified engine does not exist"))
+    if args.params:
+        custom_params = args.params
 
 def main():
     init_config()
     init_args()
-    subprocess.run(engines[selected_engine]["path"])
+    run_str = engines[selected_engine]["path"]
+    if custom_params:
+        run_str += " " + custom_params
+    subprocess.run(run_str)
 
 if __name__ == "__main__":
     main()
